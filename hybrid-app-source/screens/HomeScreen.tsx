5b8a71d06740a52ba4b4aec04ca5b30338aea0ae
David Sori <49397642+davizzx@users.noreply.github.com>
2022-05-25 23:40:41 +0200

    Use axios to fetch list of languages

--- hybrid-app-source/screens/HomeScreen.tsx
+++ hybrid-app-source/screens/HomeScreen.tsx
@@ -1,8 +1,35 @@
 import { Text, Button, View } from 'react-native'
 // @ts-ignore
 import {API_REST_URL} from 'react-native-dotenv'
+import useAxios from "axios-hooks"
 
 const Homescreen = ({ navigation }) => {
+  const [{ data: languageslist, loading, error }, refetch] = useAxios(
+    `${API_REST_URL}/languageslist`
+  );
+  if (loading)
+    return (
+      <>
+        <Text>Cargando...</Text>
+      </>
+    );
+  if (error)
+    return (
+      <>
+        <View>{JSON.stringify(error, null, 2)}</View>
+        <Button
+          onPress={() =>
+            refetch({
+              data: {
+                delay: 2,
+              },
+            })
+          }
+          title="Reintentar"
+        />
+      </>
+    );
+
   return (
     <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
       <Button title="Empezar Quiz" onPress={()=> navigation.navigate('Quiz')} />
