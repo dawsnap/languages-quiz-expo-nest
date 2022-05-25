9829bd9ceb60d6cf8dfd003e1e9504bcb0f4976e
David Sori <49397642+davizzx@users.noreply.github.com>
2022-05-25 19:28:03 +0200

    nest g module, controller and service languages

--- nest-backend-api/src/app.module.ts
+++ nest-backend-api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LanguagesModule } from './languages/languages.module';
 
 @Module({
-  imports: [],
+  imports: [LanguagesModule],
   controllers: [],
   providers: [AppService],
 })
