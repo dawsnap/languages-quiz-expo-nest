dee0f02af1ca66eeac93901386f294866fa89522
David Sori <49397642+davizzx@users.noreply.github.com>
2022-05-26 01:54:30 +0200

    Use dropdownpicker to select fetched data

--- hybrid-app-source/screens/HomeScreen.tsx
+++ hybrid-app-source/screens/HomeScreen.tsx
@@ -1,12 +1,26 @@
 import { Text, Button, View } from 'react-native'
+import { useState } from 'react';
 // @ts-ignore
 import {API_REST_URL} from 'react-native-dotenv'
 import useAxios from "axios-hooks"
+import DropDownPicker from 'react-native-dropdown-picker';
 
 const Homescreen = ({ navigation }) => {
+
   const [{ data: languageslist, loading, error }, refetch] = useAxios(
     `${API_REST_URL}/languageslist`
   );
+  console.log(languageslist)
+  const info = languageslist.map(language => {
+    return {
+      label : language.name,
+      value : language.id,
+    }
+  });
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState(info);
+  
   if (loading)
     return (
       <>
@@ -31,9 +45,38 @@ const Homescreen = ({ navigation }) => {
     );
 
   return (
-    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-      <Button title="Empezar Quiz" onPress={()=> navigation.navigate('Quiz')} />
+    <>
+    <View
+    style={{
+      backgroundColor: '#171717',
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingHorizontal: 15
+    }}
+    >
+      <Text
+      style={{
+        marginBottom: 15,
+        fontSize: 20,
+        color: 'white',
+        alignItems: 'center'
+      }}
+      >{`Selecciona un idioma para comenzar`}</Text>
+      <DropDownPicker
+      open={open}
+      value={value}
+      items={items}
+      setOpen={setOpen}
+      setValue={setValue}
+      setItems={setItems}
+      theme="DARK"
+    />
     </View>
+     <Button
+     title={`Empezar Quiz`}
+     onPress={() => navigation.navigate('Quiz')}
+   /></>
   );
 }
 
