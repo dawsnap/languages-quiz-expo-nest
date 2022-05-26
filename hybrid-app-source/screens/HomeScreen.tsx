69da5a1b1912adfdbf461d7142c07b87351aa93b
David Sori <49397642+davizzx@users.noreply.github.com>
2022-05-26 18:01:51 +0200

    Reestructured async api call logic

--- hybrid-app-source/screens/HomeScreen.tsx
+++ hybrid-app-source/screens/HomeScreen.tsx
@@ -1,27 +1,40 @@
 import { Text, Button, View } from 'react-native'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 // @ts-ignore
 import {API_REST_URL} from 'react-native-dotenv'
 import useAxios from "axios-hooks"
 import DropDownPicker from 'react-native-dropdown-picker';
+import axios from 'axios';
 
 const Homescreen = ({ navigation }) => {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setloading] = useState(true);
 
-  const [{ data: languageslist, loading, error }, refetch] = useAxios(
-    `${API_REST_URL}/languageslist`
-  );
-  console.log(languageslist)
-  const info = languageslist.map(language => {
-    return {
-      label : language.name,
-      value : language.id,
-    }
-  });
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState(null);
-  const [items, setItems] = useState(info);
-  
-  if (loading)
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(`${API_REST_URL}/languageslist`);
+      setResponse(response.data);
+    };
+
+    fetchData();
+  }, []);
+
+  if (response && items.length === 0) {
+    const info = response.map((language) => {
+      return {
+        label: language.name,
+        value: language.id,
+      };
+    });
+    setItems(info);
+  }
+
+  if (!response)
     return (
       <>
         <Text>Cargando...</Text>
@@ -31,53 +44,44 @@ const Homescreen = ({ navigation }) => {
     return (
       <>
         <View>{JSON.stringify(error, null, 2)}</View>
-        <Button
-          onPress={() =>
-            refetch({
-              data: {
-                delay: 2,
-              },
-            })
-          }
-          title="Reintentar"
-        />
       </>
     );
 
   return (
     <>
-    <View
-    style={{
-      backgroundColor: '#171717',
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingHorizontal: 15
-    }}
-    >
-      <Text
-      style={{
-        marginBottom: 15,
-        fontSize: 20,
-        color: 'white',
-        alignItems: 'center'
-      }}
-      >{`Selecciona un idioma para comenzar`}</Text>
-      <DropDownPicker
-      open={open}
-      value={value}
-      items={items}
-      setOpen={setOpen}
-      setValue={setValue}
-      setItems={setItems}
-      theme="DARK"
-    />
-    </View>
-     <Button
-     title={`Empezar Quiz`}
-     onPress={() => navigation.navigate('Quiz')}
-   /></>
+      <View
+        style={{
+          backgroundColor: '#171717',
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          paddingHorizontal: 15
+        }}
+      >
+        <Text
+          style={{
+            marginBottom: 15,
+            fontSize: 20,
+            color: 'white',
+            alignItems: 'center'
+          }}
+        >{`Selecciona un idioma para comenzar`}</Text>
+        <DropDownPicker
+          open={open}
+          value={value}
+          items={items}
+          setOpen={setOpen}
+          setValue={setValue}
+          setItems={setItems}
+          theme="DARK"
+        />
+      </View>
+      <Button
+        title={`Empezar Quiz`}
+        onPress={() => navigation.navigate('Quiz')}
+      />
+    </>
   );
-}
+};
 
 export default Homescreen;
