5666eac431befd70558eac01308ec8ea6dd1ef97
David Sori <49397642+davizzx@users.noreply.github.com>
2022-05-30 23:37:50 +0200

    Start quiz fetchs api for quiz

--- hybrid-app-source/screens/StartQuiz.tsx
+++ hybrid-app-source/screens/StartQuiz.tsx
@@ -1,14 +1,29 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Text, View } from 'react-native'
+// @ts-ignore
+import {API_REST_URL} from 'react-native-dotenv'
 
 
 const StartQuiz = ({ route, navigation }) => {
 
   const { selectedQuizId } = route.params;
 
+  const [response, setResponse] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(`${API_REST_URL}/listOfWords/${selectedQuizId}`);
+      setResponse(response.data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
       <Text>
-        Empecemos! {selectedQuizId}
+        Empecemos! {selectedQuizId} {JSON.stringify(response)}
       </Text>
     </View>
   );
