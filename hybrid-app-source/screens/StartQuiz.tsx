19f016d7843f4de4592df69bddc1bbc99cb40e40
David Sori <49397642+davizzx@users.noreply.github.com>
2022-06-01 00:18:29 +0200

    Map response to Quiz array

--- hybrid-app-source/screens/StartQuiz.tsx
+++ hybrid-app-source/screens/StartQuiz.tsx
@@ -11,6 +11,9 @@ const StartQuiz = ({ route, navigation }) => {
 
   const [response, setResponse] = useState(null);
 
+  const [quizQuestions, setQuizQuestions] = useState([]);
+  const [quizAnswers, setQuizAnswers] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       const response = await axios.get(`${API_REST_URL}/listOfWords/${selectedQuizId}`);
@@ -20,12 +23,45 @@ const StartQuiz = ({ route, navigation }) => {
     fetchData();
   }, []);
 
+  if (response && quizQuestions.length === 0) {
+    const Quiz = response.map(question =>{
+      return {
+        question : question.rightQuestion.meaning,
+        answers: 
+          [{id:question.rightQuestion.id, word:question.rightQuestion.word, right:true},
+          question.wrongAnswers.map(answer => {
+            return {id:answer.id, word:answer.word}
+          })],
+        }
+      })
+      setQuizQuestions(Quiz);
+    };
+
+  if (!response)
   return (
+    <>
+     <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 15
+      }}
+      >
+      <Text>Cargando....{`${API_REST_URL}`}</Text>
+    </View>
+    </>
+  );
+
+  return (
+    <>
     <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
       <Text>
-        Empecemos! {selectedQuizId} {JSON.stringify(response)}
+        {JSON.stringify(quizQuestions)}
       </Text>
+
     </View>
+    </>
   );
 }
 
