b045b36802c40ecf25935a1721e605bee447565b
David Sori <49397642+davizzx@users.noreply.github.com>
2022-06-01 19:59:09 +0200

    Progressbar restart logic

--- hybrid-app-source/components/ProgressBar.tsx
+++ hybrid-app-source/components/ProgressBar.tsx
@@ -8,16 +8,33 @@ const ProgressBar = (props) => {
       inputRange: [0, 100],
       outputRange: ["0%", `100%`],
   });
+
+  const animation = Animated.timing(barWidth, {
+    duration: 8000,
+    toValue: 0,
+    useNativeDriver: false
+})
+
   useEffect(() => {
   
-      Animated.timing(barWidth, {
-          duration: 8000,
-          toValue: 0,
-          useNativeDriver: false
-      }).start();
-
+     animation.start();
 
   }, [])
+
+  const restartProgressBar = () => {
+    console.log(barWidth)
+    barWidth.setValue(100)
+    animation.reset()
+    animation.start();
+  }
+  
+
+  progressPercent.addListener((percentage) => {
+      
+      if (percentage.value  == "0%"){
+        restartProgressBar();
+      }
+  })
   
     return (
         <>
