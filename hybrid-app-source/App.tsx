f99555abe0d9b25c5a34a2fd644bca94228e41e8
David Sori <49397642+davizzx@users.noreply.github.com>
2022-06-02 00:45:40 +0200

    Prevent going back and instead ask to close app

--- hybrid-app-source/App.tsx
+++ hybrid-app-source/App.tsx
@@ -4,14 +4,37 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen'
 import StartQuiz from './screens/StartQuiz'
 import Finish from './screens/Finish'
+import { Alert, BackHandler } from 'react-native';
+import { useEffect } from 'react';
 
 const Stack = createNativeStackNavigator();
 
 
 const App = () => {
+
+  useEffect(() => {
+    const backAction = () => {
+      Alert.alert("Salir", "¿Seguro que quieres cerrar la aplicación?", [
+        {
+          text: "Cancelar",
+          onPress: () => null,
+        },
+        { text: "Salir", onPress: () => BackHandler.exitApp() }
+      ]);
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
+    );
+
+    return () => backHandler.remove();
+  }, []);
+  
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Navigator screenOptions={{headerShown: false, gestureEnabled:false}}>
         <Stack.Screen
           name="Home"
           component={HomeScreen}
