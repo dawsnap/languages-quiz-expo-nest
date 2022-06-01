4a635152deb8e9ce21367b3731d9846f0de2e7f6
David Sori <49397642+davizzx@users.noreply.github.com>
2022-06-01 23:50:43 +0200

    Finish screen initial layout

--- hybrid-app-source/screens/Finish.tsx
+++ hybrid-app-source/screens/Finish.tsx
@@ -11,5 +11,32 @@ const Finish = ({ route, navigation }) => {
 
   return (
     <>
+    <SafeAreaView
+      style={{
+        flex:1,
+        backgroundColor: '#171717'
+      }}>
+        <View
+          style={{
+            backgroundColor: '#171717',
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+            paddingHorizontal: 15
+          }}
+        >
+          <Text
+            style={{
+              marginBottom: 15,
+              fontSize: 20,
+              color: '#bfc7d5',
+              alignItems: 'center'
+            }}
+          >Tu puntuación es de {score} puntos</Text>
+        </View>
+    </SafeAreaView>
+    </>
+  );
+};
 
 export default Finish;
