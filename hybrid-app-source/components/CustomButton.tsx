3ce536ac044d3824cc3818bfdef48e9c849272c4
David Sori <49397642+davizzx@users.noreply.github.com>
2022-06-02 19:04:55 +0200

    Custom reusable button component

---
+++ hybrid-app-source/components/CustomButton.tsx
@@ -0,0 +1,34 @@
+import { View, Text, Pressable } from "react-native";
+
+
+const CustomButton = (props) => {
+
+    return (
+        <Pressable
+        style={({ pressed }) => [ {
+            backgroundColor: pressed
+              ? '#292d3e50'
+              : '#292d3e',
+            borderColor:"#000",
+            borderWidth: 1,
+            alignItems: 'center',
+            justifyContent: 'space-around',
+            paddingTop: 20,
+            paddingBottom: 20
+            
+          }]}
+          onPress={props.onPress}
+        >
+        <View>
+
+        <Text style={
+            {fontSize: 15, 
+             color: '#bfc7d5'
+            }}>{props.buttonText}</Text>
+        </View>
+        </Pressable>
+    )
+
+}
+
+export default CustomButton;
