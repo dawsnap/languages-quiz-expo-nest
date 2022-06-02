ce099a2d4f32999d94fc056d17cdd64a57fdb86f
David Sori <49397642+davizzx@users.noreply.github.com>
2022-06-02 23:50:20 +0200

    Styling rankingcard

--- hybrid-app-source/components/RankingCard.tsx
+++ hybrid-app-source/components/RankingCard.tsx
@@ -2,12 +2,29 @@ import { View, Text, Pressable } from "react-native";
 
 const RankingCard = (props) => {
 
-    const { score } = props;
+    const { score, index } = props;
 
     return (
         <>
-        <View>
-            <Text>{JSON.stringify(props.score)}</Text>
+        <View 
+            style={{
+             backgroundColor:'#bfc7d599',
+             marginVertical: 2,
+             flexDirection: 'row'
+            }}>
+
+        <View style={{
+        flex: 0.3,
+        }}><Text>{index + 1}</Text></View>
+        <View style={{
+          flex: 1.7,
+        }}><Text>{score.username}</Text></View>
+        <View style={{
+          flex: 1.5,
+        }}>
+            <Text>{score.score} puntos</Text>
+            <Text>{score.finish_time}</Text>
+        </View>
         </View>
         </>
     )
