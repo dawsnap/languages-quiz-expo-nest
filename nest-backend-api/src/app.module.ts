14792788f20a0ec14d8742802b64efdae3a9c36c
David Sori <49397642+davizzx@users.noreply.github.com>
2022-06-02 19:55:39 +0200

    Changed folder

--- nest-backend-api/src/app.module.ts
+++ nest-backend-api/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { LanguagesModule } from './languages/languages.module';
+import { LanguagesModule } from './languages.module';
 import { PrismaService } from './prisma.service';
 
 @Module({
--- nest-backend-api/src/languages.module.ts
+++ nest-backend-api/src/languages.module.ts
@@ -1,4 +1,4 @@
-import { PrismaService } from './../prisma.service';
+import { PrismaService } from './prisma.service';
 import { Module } from '@nestjs/common';
 import { LanguagesController } from './languages.controller';
 import { LanguagesService } from './languages.service';
