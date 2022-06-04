92bf3c548dd0ed3b88e74519d3c0871a8127c2ad
David Sori <49397642+davizzx@users.noreply.github.com>
2022-06-04 18:22:52 +0200

    Added a ton of words

--- nest-backend-api/prisma/languages_words.ts
+++ nest-backend-api/prisma/languages_words.ts
@@ -2,7 +2,7 @@ export const languages_words = [
   {
     language_id: 1,
     word: 'Leaf',
-    meaning: 'Caterpillar',
+    meaning: 'Hoja',
   },
   {
     language_id: 1,
@@ -248,5 +248,1035 @@ export const languages_words = [
     language_id: 1,
     word: 'Focus',
     meaning: 'Enfocar',
+  },
+  {
+    language_id: 1,
+    word: 'Dilute',
+    meaning: 'Diluir',
+  },
+  {
+    language_id: 1,
+    word: 'Fine',
+    meaning: 'Multa',
+  },
+  {
+    language_id: 1,
+    word: 'Effective',
+    meaning: 'Efectivo',
+  },
+  {
+    language_id: 1,
+    word: 'Devote',
+    meaning: 'Dedicar',
+  },
+  {
+    language_id: 1,
+    word: 'Absence',
+    meaning: 'Ausencia',
+  },
+  {
+    language_id: 1,
+    word: 'Improve',
+    meaning: 'Mejorar',
+  },
+  {
+    language_id: 1,
+    word: 'Sting',
+    meaning: 'Picadura',
+  },
+  {
+    language_id: 1,
+    word: 'Consumer',
+    meaning: 'Consumidor',
+  },
+  {
+    language_id: 1,
+    word: 'Remark',
+    meaning: 'Observación',
+  },
+  {
+    language_id: 1,
+    word: 'Factory',
+    meaning: 'Fábrica',
+  },
+  {
+    language_id: 1,
+    word: 'Measurement',
+    meaning: 'Medición',
+  },
+  {
+    language_id: 1,
+    word: 'Love',
+    meaning: 'Amor',
+  },
+  {
+    language_id: 1,
+    word: 'Mixture',
+    meaning: 'Mezcla',
+  },
+  {
+    language_id: 1,
+    word: 'Writing',
+    meaning: 'Escritura',
+  },
+  {
+    language_id: 1,
+    word: 'Farmer',
+    meaning: 'Granjero',
+  },
+  {
+    language_id: 1,
+    word: 'Blood',
+    meaning: 'Sangre',
+  },
+  {
+    language_id: 1,
+    word: 'Cheek',
+    meaning: 'Mejilla',
+  },
+  {
+    language_id: 1,
+    word: 'Shirt',
+    meaning: 'Camisa',
+  },
+  {
+    language_id: 1,
+    word: 'Weakness',
+    meaning: 'Debilidad',
+  },
+  {
+    language_id: 1,
+    word: 'Steak',
+    meaning: 'Filete',
+  },
+  {
+    language_id: 1,
+    word: 'Signature',
+    meaning: 'Firma',
+  },
+  {
+    language_id: 1,
+    word: 'Buyer',
+    meaning: 'Comprador',
+  },
+  {
+    language_id: 1,
+    word: 'Wish',
+    meaning: 'Deseo',
+  },
+  {
+    language_id: 1,
+    word: 'Wake up',
+    meaning: 'Despertar',
+  },
+  {
+    language_id: 1,
+    word: 'Think',
+    meaning: 'Pensar',
+  },
+  {
+    language_id: 1,
+    word: 'Apologize',
+    meaning: 'Disculparse',
+  },
+  {
+    language_id: 1,
+    word: 'Mistake',
+    meaning: 'Error',
+  },
+  {
+    language_id: 1,
+    word: 'Blueberry',
+    meaning: 'Arándano',
+  },
+  {
+    language_id: 1,
+    word: 'Honey',
+    meaning: 'Miel',
+  },
+  {
+    language_id: 1,
+    word: 'Sentence',
+    meaning: 'Frase',
+  },
+  {
+    language_id: 1,
+    word: 'Sad',
+    meaning: 'Triste',
+  },
+  {
+    language_id: 1,
+    word: 'Chapter',
+    meaning: 'Capítulo',
+  },
+  {
+    language_id: 1,
+    word: 'Garlic',
+    meaning: 'Ajo',
+  },
+  {
+    language_id: 1,
+    word: 'Thunderstorm',
+    meaning: 'Tormenta eléctrica',
+  },
+  {
+    language_id: 1,
+    word: 'Ice-cream',
+    meaning: 'Helado',
+  },
+  {
+    language_id: 1,
+    word: 'Closet',
+    meaning: 'Armario',
+  },
+  {
+    language_id: 1,
+    word: 'Laptop',
+    meaning: 'Ordenador portátil',
+  },
+  {
+    language_id: 1,
+    word: 'Singer',
+    meaning: 'Cantante',
+  },
+  {
+    language_id: 1,
+    word: 'Deer',
+    meaning: 'Ciervo',
+  },
+  {
+    language_id: 1,
+    word: 'Last',
+    meaning: 'Último',
+  },
+  {
+    language_id: 1,
+    word: 'Reject',
+    meaning: 'Rechazar',
+  },
+  {
+    language_id: 1,
+    word: 'Brother',
+    meaning: 'Hermano',
+  },
+  {
+    language_id: 1,
+    word: 'Fail',
+    meaning: 'Fallar',
+  },
+  {
+    language_id: 1,
+    word: 'Swing',
+    meaning: 'Balanceo',
+  },
+  {
+    language_id: 1,
+    word: 'Salty',
+    meaning: 'Salado',
+  },
+  {
+    language_id: 1,
+    word: 'Sugar',
+    meaning: 'Azúcar',
+  },
+  {
+    language_id: 1,
+    word: 'Switch',
+    meaning: 'Cambiar',
+  },
+  {
+    language_id: 1,
+    word: 'Smile',
+    meaning: 'Sonrisa',
+  },
+  {
+    language_id: 1,
+    word: 'Trust',
+    meaning: 'Confiar',
+  },
+  {
+    language_id: 1,
+    word: 'Wait',
+    meaning: 'Esperar',
+  },
+  {
+    language_id: 1,
+    word: 'Remove',
+    meaning: 'Quitar',
+  },
+  {
+    language_id: 1,
+    word: 'Cap',
+    meaning: 'Gorra',
+  },
+  {
+    language_id: 1,
+    word: 'Pretend',
+    meaning: 'Fingir',
+  },
+  {
+    language_id: 1,
+    word: 'Tidy',
+    meaning: 'Ordenado',
+  },
+  {
+    language_id: 1,
+    word: 'Blind',
+    meaning: 'Ciego',
+  },
+  {
+    language_id: 1,
+    word: 'Tie',
+    meaning: 'Corbata',
+  },
+  {
+    language_id: 1,
+    word: 'Show',
+    meaning: 'Enseñar',
+  },
+  {
+    language_id: 1,
+    word: 'Shoe',
+    meaning: 'Zapato',
+  },
+  {
+    language_id: 1,
+    word: 'Gum',
+    meaning: 'Goma',
+  },
+  {
+    language_id: 1,
+    word: 'Chewing gum',
+    meaning: 'Chicle',
+  },
+  {
+    language_id: 1,
+    word: 'Glass',
+    meaning: 'Cristal',
+  },
+  {
+    language_id: 1,
+    word: 'Wood',
+    meaning: 'Madera',
+  },
+  {
+    language_id: 1,
+    word: 'Wink',
+    meaning: 'Guiño',
+  },
+  {
+    language_id: 1,
+    word: 'Gift',
+    meaning: 'Regalo',
+  },
+  {
+    language_id: 1,
+    word: 'Challenge',
+    meaning: 'Desafío',
+  },
+  {
+    language_id: 1,
+    word: 'Protect',
+    meaning: 'Proteger',
+  },
+  {
+    language_id: 1,
+    word: 'Remember',
+    meaning: 'Recordar',
+  },
+  {
+    language_id: 1,
+    word: 'Miss',
+    meaning: 'Perder',
+  },
+  {
+    language_id: 1,
+    word: 'Hot',
+    meaning: 'Caliente',
+  },
+  {
+    language_id: 1,
+    word: 'Shelter',
+    meaning: 'Refugio',
+  },
+  {
+    language_id: 1,
+    word: 'Awesome',
+    meaning: 'Alucinante',
+  },
+  {
+    language_id: 1,
+    word: 'Asleep',
+    meaning: 'Dormido',
+  },
+  {
+    language_id: 1,
+    word: 'Ashamed',
+    meaning: 'Avergonzado',
+  },
+  {
+    language_id: 1,
+    word: 'Borrow',
+    meaning: 'Tomar prestado',
+  },
+  {
+    language_id: 1,
+    word: 'Rude',
+    meaning: 'Grosero',
+  },
+  {
+    language_id: 1,
+    word: 'Town',
+    meaning: 'Pueblo',
+  },
+  {
+    language_id: 1,
+    word: 'Look',
+    meaning: 'Mirar',
+  },
+  {
+    language_id: 1,
+    word: 'Foam',
+    meaning: 'Espuma',
+  },
+  {
+    language_id: 1,
+    word: 'Board',
+    meaning: 'Tablero',
+  },
+  {
+    language_id: 1,
+    word: 'Cut',
+    meaning: 'Cortar',
+  },
+  {
+    language_id: 1,
+    word: 'Queue',
+    meaning: 'Cola',
+  },
+  {
+    language_id: 1,
+    word: 'Stir',
+    meaning: 'Remover',
+  },
+  {
+    language_id: 1,
+    word: 'Rights',
+    meaning: 'Derechos',
+  },
+  {
+    language_id: 1,
+    word: 'Left',
+    meaning: 'Izquierda',
+  },
+  {
+    language_id: 1,
+    word: 'Glow',
+    meaning: 'Brillo',
+  },
+  {
+    language_id: 1,
+    word: 'Support',
+    meaning: 'Apoyo',
+  },
+  {
+    language_id: 1,
+    word: 'Unable',
+    meaning: 'Incapaz',
+  },
+  {
+    language_id: 1,
+    word: 'Stay',
+    meaning: 'Quedarse',
+  },
+  {
+    language_id: 1,
+    word: 'Retire',
+    meaning: 'Jubilarse',
+  },
+  {
+    language_id: 1,
+    word: 'Vein',
+    meaning: 'Vena',
+  },
+  {
+    language_id: 1,
+    word: 'Proud',
+    meaning: 'Orgullo',
+  },
+  {
+    language_id: 1,
+    word: 'Wrong',
+    meaning: 'Erróneo',
+  },
+  {
+    language_id: 1,
+    word: 'Debt',
+    meaning: 'Deuda',
+  },
+  {
+    language_id: 1,
+    word: 'Valuable',
+    meaning: 'Valioso',
+  },
+  {
+    language_id: 1,
+    word: 'Irritating',
+    meaning: 'Molesto',
+  },
+  {
+    language_id: 1,
+    word: 'Worthless',
+    meaning: 'Sin valor',
+  },
+  {
+    language_id: 1,
+    word: 'Announce',
+    meaning: 'Anunciar',
+  },
+  {
+    language_id: 1,
+    word: 'Death',
+    meaning: 'Muerte',
+  },
+  {
+    language_id: 1,
+    word: 'Match',
+    meaning: 'Cerilla',
+  },
+  {
+    language_id: 1,
+    word: 'Groovy',
+    meaning: 'Maravilloso',
+  },
+  {
+    language_id: 1,
+    word: 'Pain',
+    meaning: 'Dolor',
+  },
+  {
+    language_id: 1,
+    word: 'Common',
+    meaning: 'Común',
+  },
+  {
+    language_id: 1,
+    word: 'Different',
+    meaning: 'Diluir',
+  },
+  {
+    language_id: 1,
+    word: 'Assorted',
+    meaning: 'Clasificado',
+  },
+  {
+    language_id: 1,
+    word: 'Bitter',
+    meaning: 'Amargo',
+  },
+  {
+    language_id: 1,
+    word: 'Sweet',
+    meaning: 'Dulce',
+  },
+  {
+    language_id: 1,
+    word: 'Purpose',
+    meaning: 'Objetivo',
+  },
+  {
+    language_id: 1,
+    word: 'Obnoxious',
+    meaning: 'Odioso',
+  },
+  {
+    language_id: 1,
+    word: 'Standing',
+    meaning: 'De pie',
+  },
+  {
+    language_id: 1,
+    word: 'Tongue',
+    meaning: 'Lengua',
+  },
+  {
+    language_id: 1,
+    word: 'War',
+    meaning: 'Guerra',
+  },
+  {
+    language_id: 1,
+    word: 'Shake',
+    meaning: 'Agitar',
+  },
+  {
+    language_id: 1,
+    word: 'Pollution',
+    meaning: 'Contaminación',
+  },
+  {
+    language_id: 1,
+    word: 'Dinner',
+    meaning: 'Cena',
+  },
+  {
+    language_id: 1,
+    word: 'Snore',
+    meaning: 'Roncar',
+  },
+  {
+    language_id: 1,
+    word: 'Torch',
+    meaning: 'Linterna',
+  },
+  {
+    language_id: 1,
+    word: 'Eye',
+    meaning: 'Ojo',
+  },
+  {
+    language_id: 1,
+    word: 'Scape',
+    meaning: 'Escapar',
+  },
+  {
+    language_id: 1,
+    word: 'Download',
+    meaning: 'Descargar',
+  },
+  {
+    language_id: 1,
+    word: 'Make-up',
+    meaning: 'Maquillaje',
+  },
+  {
+    language_id: 1,
+    word: 'Bakery',
+    meaning: 'Panadería',
+  },
+  {
+    language_id: 1,
+    word: 'Help',
+    meaning: 'Ayudar',
+  },
+  {
+    language_id: 1,
+    word: 'Pescado',
+    meaning: 'Fish',
+  },
+  {
+    language_id: 1,
+    word: 'Angry',
+    meaning: 'Enfadado',
+  },
+  {
+    language_id: 1,
+    word: 'Light',
+    meaning: 'Luz',
+  },
+  {
+    language_id: 1,
+    word: 'Dark',
+    meaning: 'Oscuro',
+  },
+  {
+    language_id: 1,
+    word: 'Finish',
+    meaning: 'Acabar',
+  },
+  {
+    language_id: 1,
+    word: 'Season',
+    meaning: 'Temporada',
+  },
+  {
+    language_id: 1,
+    word: 'Sea',
+    meaning: 'Mar',
+  },
+  {
+    language_id: 1,
+    word: 'Swim',
+    meaning: 'Nadar',
+  },
+  {
+    language_id: 1,
+    word: 'Rotten',
+    meaning: 'Podrido',
+  },
+  {
+    language_id: 1,
+    word: 'Translate',
+    meaning: 'Traducir',
+  },
+  {
+    language_id: 1,
+    word: 'Guilty',
+    meaning: 'Culpable',
+  },
+  {
+    language_id: 1,
+    word: 'Steam',
+    meaning: 'Vapor',
+  },
+  {
+    language_id: 1,
+    word: 'Travel',
+    meaning: 'Viajar',
+  },
+  {
+    language_id: 1,
+    word: 'Phone',
+    meaning: 'Teléfono',
+  },
+  {
+    language_id: 1,
+    word: 'Number',
+    meaning: 'Número',
+  },
+  {
+    language_id: 1,
+    word: 'Elevator',
+    meaning: 'Ascensor',
+  },
+  {
+    language_id: 1,
+    word: 'Socks',
+    meaning: 'Calcetines',
+  },
+  {
+    language_id: 1,
+    word: 'Crazy',
+    meaning: 'Loco',
+  },
+  {
+    language_id: 1,
+    word: 'Coat',
+    meaning: 'Abrigo',
+  },
+  {
+    language_id: 1,
+    word: 'Lost',
+    meaning: 'Perdido',
+  },
+  {
+    language_id: 1,
+    word: 'Hammer',
+    meaning: 'Martillo',
+  },
+  {
+    language_id: 1,
+    word: 'Thing',
+    meaning: 'Cosa',
+  },
+  {
+    language_id: 1,
+    word: 'Listen',
+    meaning: 'Escuchar',
+  },
+  {
+    language_id: 1,
+    word: 'Railway',
+    meaning: 'Ferrocarril',
+  },
+  {
+    language_id: 1,
+    word: 'Unemployment',
+    meaning: 'Desempleo',
+  },
+  {
+    language_id: 1,
+    word: 'Circle',
+    meaning: 'Círculo',
+  },
+  {
+    language_id: 1,
+    word: 'Valuable',
+    meaning: 'Valioso',
+  },
+  {
+    language_id: 1,
+    word: 'Skeleton',
+    meaning: 'Esqueleto',
+  },
+  {
+    language_id: 1,
+    word: 'Seventeen',
+    meaning: 'Diecisiete',
+  },
+  {
+    language_id: 1,
+    word: 'Better',
+    meaning: 'Mejor',
+  },
+  {
+    language_id: 1,
+    word: 'Worse',
+    meaning: 'Peor',
+  },
+  {
+    language_id: 1,
+    word: 'Seat',
+    meaning: 'Asiento',
+  },
+  {
+    language_id: 1,
+    word: 'Fall',
+    meaning: 'Caer',
+  },
+  {
+    language_id: 1,
+    word: 'Reminder',
+    meaning: 'Recordatorio',
+  },
+  {
+    language_id: 1,
+    word: 'Field',
+    meaning: 'Campo',
+  },
+  {
+    language_id: 1,
+    word: 'Mouse',
+    meaning: 'Ratón',
+  },
+  {
+    language_id: 1,
+    word: 'Cucumber',
+    meaning: 'Pepino',
+  },
+  {
+    language_id: 1,
+    word: 'Eggplant',
+    meaning: 'Berenjena',
+  },
+  {
+    language_id: 1,
+    word: 'Corn',
+    meaning: 'Maíz',
+  },
+  {
+    language_id: 1,
+    word: 'Fabric',
+    meaning: 'Tejido',
+  },
+  {
+    language_id: 1,
+    word: 'Money',
+    meaning: 'Dinero',
+  },
+  {
+    language_id: 1,
+    word: 'Rope',
+    meaning: 'Cuerda',
+  },
+  {
+    language_id: 1,
+    word: 'Vase',
+    meaning: 'Jarrón',
+  },
+  {
+    language_id: 1,
+    word: 'Foot',
+    meaning: 'Pie',
+  },
+  {
+    language_id: 1,
+    word: 'Idiom',
+    meaning: 'Expresión',
+  },
+  {
+    language_id: 1,
+    word: 'Carpet',
+    meaning: 'Alfombra',
+  },
+  {
+    language_id: 1,
+    word: 'Folder',
+    meaning: 'Carpeta',
+  },
+  {
+    language_id: 1,
+    word: 'Pregnant',
+    meaning: 'Embarazada',
+  },
+  {
+    language_id: 1,
+    word: 'Introduce',
+    meaning: 'Presentar',
+  },
+  {
+    language_id: 1,
+    word: 'Soap',
+    meaning: 'Jabón',
+  },
+  {
+    language_id: 1,
+    word: 'Sick',
+    meaning: 'Enfermo',
+  },
+  {
+    language_id: 1,
+    word: 'News',
+    meaning: 'Noticia',
+  },
+  {
+    language_id: 1,
+    word: 'Lawyer',
+    meaning: 'Abogado',
+  },
+  {
+    language_id: 1,
+    word: 'Send',
+    meaning: 'Enviar',
+  },
+  {
+    language_id: 1,
+    word: 'Waitress',
+    meaning: 'Camarero',
+  },
+  {
+    language_id: 1,
+    word: 'Cashier',
+    meaning: 'Cajero',
+  },
+  {
+    language_id: 1,
+    word: 'Scientist',
+    meaning: 'Científico',
+  },
+  {
+    language_id: 1,
+    word: 'Tired',
+    meaning: 'Cansado',
+  },
+  {
+    language_id: 1,
+    word: 'Salad',
+    meaning: 'Ensalada',
+  },
+  {
+    language_id: 1,
+    word: 'Biscuit',
+    meaning: 'Galleta',
+  },
+  {
+    language_id: 1,
+    word: 'Butter',
+    meaning: 'Mantequilla',
+  },
+  {
+    language_id: 1,
+    word: 'Homemade',
+    meaning: 'Casero',
+  },
+  {
+    language_id: 1,
+    word: 'Orange',
+    meaning: 'Naranja',
+  },
+  {
+    language_id: 1,
+    word: 'Bread',
+    meaning: 'Pan',
+  },
+  {
+    language_id: 1,
+    word: 'Rice',
+    meaning: 'Arroz',
+  },
+  {
+    language_id: 1,
+    word: 'Apple',
+    meaning: 'Manzana',
+  },
+  {
+    language_id: 1,
+    word: 'Wife',
+    meaning: 'Mujer',
+  },
+  {
+    language_id: 1,
+    word: 'Husband',
+    meaning: 'Mardo',
+  },
+  {
+    language_id: 1,
+    word: 'Different',
+    meaning: 'Diferente',
+  },
+  {
+    language_id: 1,
+    word: 'Lonely',
+    meaning: 'Solitario',
+  },
+  {
+    language_id: 1,
+    word: 'Square',
+    meaning: 'Plaza',
+  },
+  {
+    language_id: 1,
+    word: 'Street',
+    meaning: 'Calle',
+  },
+  {
+    language_id: 1,
+    word: 'Green',
+    meaning: 'Verde',
+  },
+  {
+    language_id: 1,
+    word: 'Avoid',
+    meaning: 'Evitar',
+  },
+  {
+    language_id: 1,
+    word: 'Bald',
+    meaning: 'Calvo',
+  },
+  {
+    language_id: 1,
+    word: 'Faith',
+    meaning: 'Destino',
+  },
+  {
+    language_id: 1,
+    word: 'Scar',
+    meaning: 'Cicatriz',
+  },
+  {
+    language_id: 1,
+    word: 'Behaviour',
+    meaning: 'Comportamiento',
+  },
+  {
+    language_id: 1,
+    word: 'Hope',
+    meaning: 'Esperanza',
+  },
+  {
+    language_id: 1,
+    word: 'Time',
+    meaning: 'Tiempo',
+  },
+  {
+    language_id: 1,
+    word: 'Bear',
+    meaning: 'Oso',
+  },
+  {
+    language_id: 1,
+    word: 'Horse',
+    meaning: 'Caballo',
+  },
+  {
+    language_id: 1,
+    word: 'Dance',
+    meaning: 'Bailar',
+  },
+  {
+    language_id: 1,
+    word: 'Sauce',
+    meaning: 'Salsa',
   },
 ];
